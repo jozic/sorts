d46140f731aef0f561fa848352ceb43a6c2d7bf0
jozic <jozic@ua.fm>
2013-01-02 23:57:44 -0500

    stability tests (merge sort implementation is not stable)

--- src/test/scala/com/jozic/sorts/SortSpec.scala
+++ src/test/scala/com/jozic/sorts/SortSpec.scala
@@ -1,20 +1,74 @@
 package com.jozic.sorts
 
 import org.scalatest.FunSpec
+import org.scalatest.matchers.MustMatchers
 
-class SortSpec extends FunSpec {
+class SortSpec extends FunSpec with MustMatchers {
+
+  def assertSort[A: Ordering](sorter: Sort, array: Array[A]) {
+    assert(isSorted(sorter.sort(array)) === true)
+  }
+
+  def isSorted[A: Ordering](array: Array[A]): Boolean = {
+    val ord = Ordering[A]
+    import ord._
+    var i = 0
+    while (i != array.length - 1) {
+      if (array(i) > array(i + 1)) return false
+      i += 1
+    }
+    true
+  }
 
   def testCorrectness(sorter: Sort) {
-    assert(sorter.sort(Array(1, 3, 2, 5, 4)) === Array(1, 2, 3, 4, 5))
-    assert(sorter.sort(Array(1, 2, 3, 4, 5)) === Array(1, 2, 3, 4, 5))
-    assert(sorter.sort(Array(5, 4, 3, 2, 1)) === Array(1, 2, 3, 4, 5))
-    assert(sorter.sort(Array(5, 1, 2, 3, 4)) === Array(1, 2, 3, 4, 5))
-    assert(sorter.sort(Array(2, 4, 6, 1, 3, 5)) === Array(1, 2, 3, 4, 5, 6))
+    assertSort(sorter, Array(1, 3, 2, 5, 4))
+    assertSort(sorter, Array(1, 2, 3, 4, 5))
+    assertSort(sorter, Array(5, 4, 3, 2, 1))
+    assertSort(sorter, Array(5, 1, 2, 3, 4))
+    assertSort(sorter, Array(2, 4, 6, 1, 3, 5))
+  }
+
+  def assertStable(sorter: Sort, array: Array[StableCheck]) {
+    isStable(sorter.sort(array)) must be(true)
+  }
+
+  def assertNotStable(sorter: Sort, array: Array[StableCheck]) {
+    isStable(sorter.sort(array)) must be(false)
+  }
+
+  def isStable(array: Array[StableCheck]): Boolean = {
+    array.groupBy(_.value) map (g => g._2 map (_.check)) foreach {
+      a => if (!isSorted(a)) return false
+    }
+    true
+  }
+
+  def assertStable(sorter: Sort) {
+    assertStable(sorter, Array(s(1, 1), s(1, 2), s(5, 1), s(5, 2), s(3, 1), s(3, 2)))
+    assertStable(sorter, Array(s(3, 1), s(3, 2), s(1, 1), s(5, 1)))
+    assertStable(sorter, Array(s(5, 1), s(5, 2), s(3, 1), s(3, 2), s(1, 1), s(1, 2)))
+  }
+
+  def assertNotStable(sorter: Sort) {
+    assertNotStable(sorter, Array(s(1, 1), s(1, 2), s(5, 1), s(5, 2), s(3, 1), s(3, 2)))
+    assertNotStable(sorter, Array(s(3, 1), s(3, 2), s(1, 1), s(5, 1)))
+    assertNotStable(sorter, Array(s(5, 1), s(5, 2), s(3, 1), s(3, 2), s(1, 1), s(1, 2)))
+  }
+
+  case class StableCheck(value: Int, check: Int)
+
+  def s(value: Int, check: Int) = StableCheck(value, check)
+
+  implicit val StableCheckOrdering = new Ordering[StableCheck] {
+    def compare(x: SortSpec.this.type#StableCheck, y: SortSpec.this.type#StableCheck) = x.value - y.value
   }
 
   describe("Bubble sort") {
     it("should sort correctly") {
       testCorrectness(BubbleSort)
+    }
+    it("should be stable") {
+      assertStable(BubbleSort)
     }
   }
 
@@ -22,11 +76,17 @@ class SortSpec extends FunSpec {
     it("should sort correctly") {
       testCorrectness(SelectionSort)
     }
+    it("should NOT be stable") {
+      assertNotStable(SelectionSort, Array(s(3, 1), s(3, 2), s(1, 1), s(5, 1)))
+    }
   }
 
   describe("Insertion sort") {
     it("should sort correctly") {
       testCorrectness(InsertionSort)
+    }
+    it("should be stable") {
+      assertStable(InsertionSort)
     }
   }
 
@@ -34,6 +94,8 @@ class SortSpec extends FunSpec {
     it("should sort correctly") {
       testCorrectness(MergeSort)
     }
+    it("should be stable") {
+      assertStable(MergeSort)
+    }
   }
-
 }
